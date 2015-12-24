25a72412441bef78d0c82fe85da119fa4fd76017
Kenni Bawden <kenni.bawden@gmail.com>
2015-12-24 19:40:04 +1100

    Added a http server for web browser submission

--- just-add.js
+++ just-add.js
@@ -8,14 +8,17 @@ function composition(face) {
 	return ['composite', '-geometry', width+'x'+height+'+'+(face.x)+'+'+(face.y - face.height/4)];
 }
 
-function justAdd(photo, out, addFaces) {
+function justAdd(photo, out, addFaces, callback) {
 	cv.readImage(photo, function(err, image) {
 		image.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
 			image.save(out);
-			faces.forEach(function(face) {
-				var addFace = addFaces[Math.floor(Math.random() * addFaces.length)];
-				exec(composition(face).concat([addFace, out, out]).join(' '));
-			});
+			if (faces) {
+				faces.forEach(function(face) {
+					var addFace = addFaces[Math.floor(Math.random() * addFaces.length)];
+					exec(composition(face).concat([addFace, out, out]).join(' '));
+				});
+			}
+			callback();
 		});
 	});
 }
---
+++ server.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+var path = require('path');
+var express = require("express");
+var busboy = require('connect-busboy');
+var unique = require('unique-filename');
+var justAdd = require('./just-add');
+
+var server = process.argv[2] || 'localhost';
+var port = process.argv[3] || '6000';
+var addFaces = process.argv.slice(4);
+
+console.log('Server started on port '+port);
+
+var app = express();
+app.use(busboy());
+
+app.get('/', function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res);
+});
+app.post('/', function (req, res) {
+	var photo = unique('photos/')+'.png';
+	var outPhoto = unique('photos/')+'.png';
+
+	var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        fstream = fs.createWriteStream(path.join(__dirname, photo));
+        file.pipe(fstream);
+        fstream.on('close', function () {
+			justAdd(photo, outPhoto, addFaces, function() {
+				res.statusCode = 302;
+				res.setHeader('Location', '/'+outPhoto);
+				res.end();
+			});
+        });
+    });
+});
+app.get('/photos/:file', function(req, res) {
+	var photo = req.params.file;
+	res.writeHead(200, {'Content-Type': 'image/png'});
+	fs.createReadStream(path.join(__dirname, 'photos',  photo)).pipe(res);
+});
+
+app.listen(port);
